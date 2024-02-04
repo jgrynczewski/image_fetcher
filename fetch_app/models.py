c676870edcaac68143bf742ae93b00e102b2dc32
jgrynczewski <jgrynczewski@gmail.com>
2024-02-04 10:19:31 +0100

    models

--- fetch_app/models.py
+++ fetch_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Image(models.Model):
+    url = models.URLField()
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    send_to = models.ManyToManyField('Receiver', through='ImageReceiver')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Receiver(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class ImageReceiver(models.Model):
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    receiver = models.ForeignKey(Receiver, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
